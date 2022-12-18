0c62db8b7b31627412c071cec5a002b802ef2952
magnitopic <magnitopic@gmail.com>
2022-12-18 19:09:18 +0100

    generate random stacks

--- push_swap.c
+++ push_swap.c
@@ -6,20 +6,45 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 14:16:59 by alaparic          #+#    #+#             */
-/*   Updated: 2022/12/18 17:23:02 by alaparic         ###   ########.fr       */
+/*   Updated: 2022/12/18 19:08:11 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <time.h>
+#include <stdlib.h>
 #include "./libft/libft.h"
+
+static int	ft_contains(int *array, int value)
+{
+	int	flag;
+
+	flag = 0;
+	while (*array)
+	{
+		if (*array++ == value)
+			flag = 1;
+	}
+	return (flag);
+}
 
 static int	*random_nums(int amount)
 {
 	int	*nums;
+	int	i;
+	int	rnd;
 
+	i = 0;
+	srand(time(NULL));
 	nums = ft_calloc((amount + 1), sizeof(int));
-	while (amount-- != 0)
+	while (i < amount)
 	{
-		nums[amount] = 5;
+		rnd = (rand() % (amount )) + 1;
+		while (ft_contains(nums, rnd))
+		{
+			rnd = (rand() % (amount)) + 1;
+		}
+		nums[i] = rnd;
+		i++;
 	}
 	return (nums);
 }
