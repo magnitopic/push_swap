b388e930a375861aeb9115927599be15269c524f
magnitopic <magnitopic@gmail.com>
2023-01-23 19:03:58 +0100

    howSum with memoization

--- extra/pythonTesting/howSum.py
+++ extra/pythonTesting/howSum.py
@@ -1,5 +1,5 @@
 # Basic algorithm
-def howSum(target, numbers):
+""" def howSum(target, numbers):
     if target == 0:
         return []
     if target < 0:
@@ -11,8 +11,27 @@ def howSum(target, numbers):
             result.append(i)
             return result
 
+    return None """
+
+
+# Optimized algorithm
+def howSum(target, numbers, memo={}):
+    if target in memo:
+        return memo[target]
+    if target == 0:
+        return []
+    if target < 0:
+        return None
+
+    for i in numbers:
+        result = howSum(target - i, numbers, memo)
+        if result != None:
+            result.append(i)
+            memo[target] = result
+            return result
+    memo[target] = None
     return None
 
 
 if __name__ == "__main__":
-    print(howSum(5, [5, 3, 2]))
+    print(howSum(300, [7, 14]))
