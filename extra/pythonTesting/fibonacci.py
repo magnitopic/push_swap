69a68f7ce9b33042c28ba53b2e6207ee2d62b293
magnitopic <magnitopic@gmail.com>
2023-01-23 17:32:48 +0100

    gridTraveler optimized algorithm

--- extra/pythonTesting/fibonacci.py
+++ extra/pythonTesting/fibonacci.py
@@ -1,15 +1,18 @@
 # Basic algorithm
 """ def fib(n):
     if n <= 2: return 1
-    return fib(n-1) + fib(n -2)
-"""
+    return fib(n-1) + fib(n -2) """
+
 
 # Optimized algorithm
 def fib(n, values={}):
-    if n in values: return values[n]
-    if n <= 2: return 1
+    if n in values:
+        return values[n]
+    if n <= 2:
+        return 1
     values[n] = (fib(n - 1, values) + fib(n - 2, values))
     return values[n]
+
 
 if __name__ == "__main__":
     print(fib(50))
--- extra/pythonTesting/gridTraveler.py
+++ extra/pythonTesting/gridTraveler.py
@@ -1,12 +1,24 @@
 # Basic algorithm
-def gridTraveler(x, y):
+""" def gridTraveler(x, y):
+	if x == 1 and y == 1:
+		return 1
+	if x == 0 or y == 0:
+		return 0
+	return gridTraveler(x-1, y) + gridTraveler(x, y - 1) """
+
+
+# Optimized algorithm
+def gridTraveler(x, y, memo={}):
+    key = str(x)+","+str(y)
+    if key in memo:
+        return memo[key]
     if x == 1 and y == 1:
         return 1
     if x == 0 or y == 0:
         return 0
-    len = gridTraveler(x-1, y) + gridTraveler(x, y - 1)
-    return len
+    memo[key] = gridTraveler(x-1, y) + gridTraveler(x, y - 1)
+    return memo[key]
 
 
 if __name__ == "__main__":
-    print(gridTraveler(10, 15))
+    print(gridTraveler(10, 50))
