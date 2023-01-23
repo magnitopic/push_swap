1be8f21eaaf122c1f6b948c8498342d374d67da4
magnitopic <magnitopic@gmail.com>
2023-01-23 17:20:57 +0100

    gridTraveler basic

---
+++ extra/pythonTesting/gridTraveler.py
@@ -0,0 +1,11 @@
+def gridTraveler(x, y):
+    if x == 1 and y == 1:
+        return 1
+    if x == 0 or y == 0:
+        return 0
+    len = gridTraveler(x-1, y) + gridTraveler(x, y - 1)
+    return len
+
+
+if __name__ == "__main__":
+    print(gridTraveler(10, 15))
