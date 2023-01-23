e3f6b5a18040f5a7564b0205d7c2f57b1ad87054
magnitopic <magnitopic@gmail.com>
2023-01-23 17:02:19 +0100

    fibonacci with recursion

--- extra/pythonTesting/fibonacci.py
+++ extra/pythonTesting/fibonacci.py
@@ -1,17 +1,13 @@
-""" def fib(n):
-    if n == 1 or n == 2:
-        return 1
-    values = [1, 1]
-    i = 1
-    while len(values) < n:
-        values.append(values[i-1] + values[i])
-        i += 1
-    return values[i]
+def fib(n, values={}):
+    if n in values: return values[n]
+    if n <= 2: return 1
+    values[n] = (fib(n - 1, values) + fib(n - 2, values))
+    return values[n]
 
- """
-def fib(n):
+
+""" def fib(n):
     if n <= 2: return 1
     return fib(n-1) + fib(n -2)
-
+"""
 if __name__ == "__main__":
     print(fib(50))
