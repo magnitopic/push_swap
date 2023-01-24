ec0efe6adc8167ba739e4c610589362218463b93
magnitopic <magnitopic@gmail.com>
2023-01-24 12:51:16 +0100

    advanced countConstruct

--- extra/pythonTesting/countConstruct.py
+++ extra/pythonTesting/countConstruct.py
@@ -1,5 +1,5 @@
 # Basic algorithm
-def countConstruct(target, strings):
+""" def countConstruct(target, strings):
     if (target == ""):
         return 1
     totalCount = 0
@@ -9,8 +9,26 @@ def countConstruct(target, strings):
             result = countConstruct(newString, strings)
             if result > 0:
                 totalCount += result
+    return totalCount """
+
+
+# Advanced algorithm
+def countConstruct(target, strings, memo={}):
+    if target in memo:
+        return memo[target]
+    if (target == ""):
+        return 1
+    totalCount = 0
+    for i in strings:
+        if (not target.find(i)):
+            newString = target[len(i):len(target)]
+            result = countConstruct(newString, strings)
+            if result > 0:
+                totalCount += result
+    memo[target] = totalCount
     return totalCount
 
 
 if __name__ == "__main__":
-    print(countConstruct("purple", ["purp", "p", "ur", "le", "purpl"]))
+    print(countConstruct("eeeeeeeeeeeeeeeeeeeeeeeeee", [
+          "e", "ee", "eee", "eeee", "eeeee", "eeeeee", "eeeeeee"]))
