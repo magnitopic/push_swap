47afcc62544d0b2a5a7a2eff89a373798451d691
magnitopic <magnitopic@gmail.com>
2023-01-30 13:31:50 +0100

    Optimize algorithm to cutdown wait time

--- extra/pushSwap-Python/push_swap.py
+++ extra/pushSwap-Python/push_swap.py
@@ -12,51 +12,38 @@ def checkSorted(a, b):
     return True
 
 
-def decide(a, b, moves=["sa", "sb", "ss", "pa", "pb", "ra", "rb", "rr", "rra", "rrb", "r"], count=0, memo={}):
-    if str(a) in memo:
-        print(memo[a])
-    if checkSorted(a, b):
-        return []
-    if count > 4:
-        return None
+def decide(a, b, count=0, memo={}):
+    moves=["sa", "sb", "ss", "pa", "pb", "ra", "rb", "rr", "rra", "rrb", "r"]
+    #if str(a) in memo:print(memo[a])
+    if checkSorted(a, b):return []
+    if count > 7:return None
     shortestCombination = None
     for i in moves:
         aCopy = copy.deepcopy(a)
         bCopy = copy.deepcopy(b)
-        if (i == "sa"):
-            swap(aCopy)
-        elif (i == "sb"):
-            swap(bCopy)
-        elif (i == "ss"):
-            ss(aCopy, bCopy)
-        elif (i == "pa"):
-            push(aCopy, bCopy)
-        elif (i == "pb"):
-            push(bCopy, aCopy)
-        elif (i == "ra"):
-            rotate(aCopy)
-        elif (i == "rb"):
-            rotate(bCopy)
-        elif (i == "rr"):
-            rr(aCopy, bCopy)
-        elif (i == "rra"):
-            reverse_rotate(aCopy)
-        elif (i == "rrb"):
-            reverse_rotate(bCopy)
-        elif (i == "rrr"):
-            rrr(aCopy, bCopy)
-        result = decide(aCopy, bCopy, moves, count + 1)
-        if result != None:
-            result.append(i)
-            if shortestCombination == None or len(result) < len(shortestCombination):
-                shortestCombination = result
+        if (i == "sa"):swap(aCopy)
+        elif (i == "sb"):swap(bCopy)
+        elif (i == "ss"):ss(aCopy, bCopy)
+        elif (i == "pa"):push(aCopy, bCopy)
+        elif (i == "pb"):push(bCopy, aCopy)
+        elif (i == "ra"):rotate(aCopy)
+        elif (i == "rb"):rotate(bCopy)
+        elif (i == "rr"):rr(aCopy, bCopy)
+        elif (i == "rra"):reverse_rotate(aCopy)
+        elif (i == "rrb"):reverse_rotate(bCopy)
+        elif (i == "rrr"):rrr(aCopy, bCopy)
+        if not(a == aCopy and b == bCopy):
+            remainderCombination = decide(aCopy, bCopy, count + 1)
+            if remainderCombination is not None:
+                combination = [*remainderCombination, i]
+                if shortestCombination is None or len(combination) < len(shortestCombination):
+                    shortestCombination = combination
     #memo[str(a)] = str(shortestCombination)
-    print([str(a).replace(char, '') for char in "[],"]) 
     return shortestCombination
 
 
 def push_swap():
-    a = [2, 0, 1]
+    a = [2, 0, -1]
     b = []
     for i in decide(a, b):
         print(i)
