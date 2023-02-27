31e7c29dccb03be8bc93b8f2638b4c1d747b0cd2
magnitopic <magnitopic@gmail.com>
2023-02-27 18:15:54 +0100

    may loops

--- src/dictionary/dict_get.c
+++ src/dictionary/dict_get.c
@@ -6,12 +6,11 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 17:16:46 by alaparic          #+#    #+#             */
-/*   Updated: 2023/02/27 17:19:24 by alaparic         ###   ########.fr       */
+/*   Updated: 2023/02/27 17:45:53 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/push_swap.h"
-
 
 t_entry	*dict_get(t_entry *dict, int pos)
 {
