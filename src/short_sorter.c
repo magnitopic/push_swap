9a9c5a6ec08fe253f7222c664cbbd7ff4bc3f5c8
magnitopic <magnitopic@gmail.com>
2023-03-03 18:45:44 +0100

    new stop logic

--- src/short_sorter.c
+++ src/short_sorter.c
@@ -6,7 +6,7 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/25 15:29:01 by alaparic          #+#    #+#             */
-/*   Updated: 2023/03/02 17:19:26 by alaparic         ###   ########.fr       */
+/*   Updated: 2023/03/03 18:27:29 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,14 @@ char	*busca(t_list *stack_a, t_list *stack_b)
 	t_entry	*dictionary;
 	int		i;
 	int		j;
+	int		k;
 	t_list	*stack_a_cpy;
 	t_list	*stack_b_cpy;
 	char	*move;
 	//char	*(*moves[11])(t_list stack_a, t_list stack_b);
 
 	j = 0;
+	k = 0;
 	dictionary = dict_new(stack_a, stack_b, "");
 	while (1)
 	{
@@ -30,10 +32,8 @@ char	*busca(t_list *stack_a, t_list *stack_b)
 		while (i < 11)
 		{
 			move = "";
-			stack_a_cpy = ft_lstcpy(dict_get(dictionary, j)->stack_a);
-			stack_b_cpy = ft_lstcpy(dict_get(dictionary, j)->stack_b);
-			if (ft_issorted(stack_a_cpy, stack_b_cpy))
-				return (dict_get(dictionary, i + j)->moves);
+			stack_a_cpy = ft_lstcpy(dict_get(dictionary, k)->stack_a);
+			stack_b_cpy = ft_lstcpy(dict_get(dictionary, k)->stack_b);
 			if (i == 0)
 				move = sa(&stack_a_cpy, &stack_b_cpy);
 			else if (i == 1)
@@ -56,19 +56,18 @@ char	*busca(t_list *stack_a, t_list *stack_b)
 				move = rrb(&stack_a_cpy, &stack_b_cpy);
 			else if (i == 10)
 				move = rrr(&stack_a_cpy, &stack_b_cpy);
+			if (ft_is_in_function(dictionary, stack_a_cpy, stack_b_cpy) && ft_strlen(move))
+				dict_add_back(&dictionary, dict_new(stack_a_cpy, stack_b_cpy, ft_strjoin(dict_get(dictionary, k)->moves, move)));
+			ft_printf("i: %d j: %d k: %d\n",i, j, k);
+			ft_lstprint_int(stack_a_cpy);
+			ft_lstprint_int(stack_b_cpy);
+			if (ft_issorted(stack_a_cpy, stack_b_cpy))
+				return (dict_get(dictionary, dict_size(dictionary) - 1)->moves);
 			i++;
-			if (ft_is_in_function(dictionary, stack_a_cpy, stack_b_cpy))
-				dict_add_back(&dictionary, dict_new(stack_a_cpy, stack_b_cpy, ft_strjoin(dict_get(dictionary, j)->moves, move)));
+			j++;
 		}
-		ft_printf("===================");
-		i = 0;
-		while (i < dict_size(dictionary))
-		{
-			ft_lstprint_int(dict_get(dictionary, i)->stack_a);
-			ft_lstprint_int(dict_get(dictionary, i)->stack_b);
-			i++;
-		}
-		j++;
+		k++;
+		ft_printf("===========\n");
 	}
 	return ("");
 }
